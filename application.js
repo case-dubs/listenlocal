8490335d5cc2be9f5ae0304b4ecd5b448b3fb6d0
case-dubs <casey594@gmail.com>
2013-09-25 15:37:52 -0700

    for willis' review

--- application.js
+++ application.js
@@ -1,8 +1,24 @@
+//Tomorrow AM: create manual api callback. Once that works, create automated url based off of band name
+
+	console.log("document loads!");
 
 
-	alert("document loads!");
-	
 	function submit(){
-		alert("Submit button works");
+		console.log("Submit button works");
+		var artistField = document.getElementById('artistName');
+		var artistName = artistField.value;
+		var spotifyArtistApi = 'http://ws.spotify.com/search/1/artist?q='
+
+		$.getJSON(spotifyArtistApi, {
+			format: 'json',
+			data: data,
+			url: spotifyArtistApi + 'artistName',
+		})
+		.done(function(data){
+			console.log("json callback done");
+			console.log(data);
+		});
 	};
+
+	
 	
