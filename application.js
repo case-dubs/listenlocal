17c2a3771e25ae447ae4d71695bfe5b8308acb3b
case-dubs <casey594@gmail.com>
2013-10-09 13:55:42 -0700

    pre class push

--- application.js
+++ application.js
@@ -42,24 +42,44 @@
      	  $.getJSON(skEventSearchAPI, function(skEventsReturned){
      	  	console.log("this was returned from the SK API call: ");
      	  	console.dir(skEventsReturned);
-     	  	var skEventHeadline = skEventsReturned.resultsPage.results.event[0].displayName;
-     	  	var skArtistName = skEventsReturned.resultsPage.results.event[0].performance[0].artist.displayName;
-     	  	var eventVenue = skEventsReturned.resultsPage.results.event[0].venue.displayName;
-     	  	var eventDate = skEventsReturned.resultsPage.results.event[0].start.date;
-     	  	var eventTime = skEventsReturned.resultsPage.results.event[0].start.time;
-     	  	var eventLocation = skEventsReturned.resultsPage.results.event[0].location.city;
+     	  	
+     	  	var eventsLength = skEventsReturned.resultsPage.results.event.length;
+     	  	var loopCounter = "";
+
+//TODO: need to add instances for eventsLength = 0 and 1-19
+
+     	  	if (eventsLength > 20){
+     	  			loopCounter = 20;
+     	  		}else{
+     	  			loopCounter = eventsLength;
+     	  		}
+
+     	  	console.log("this is loopcounter's value: " + loopCounter);
+
+     	  	for (i = 0; i<loopCounter; i++){
+
+     	  	console.log("counter =" + i);
+     	  	console.dir(skEventsReturned.resultsPage.results.event[i]);
+     	  	
+
+    	  	var skEventHeadline = skEventsReturned.resultsPage.results.event[i].displayName;
+     	  	var skArtistName = skEventsReturned.resultsPage.results.event[i].performance[0].artist.displayName;
+     	  	var eventVenue = skEventsReturned.resultsPage.results.event[i].venue.displayName;
+     	  	var eventDate = skEventsReturned.resultsPage.results.event[i].start.date;
+     	  	var eventTime = skEventsReturned.resultsPage.results.event[i].start.time;
+     	  	var eventLocation = skEventsReturned.resultsPage.results.event[i].location.city;
      	  	console.log(skArtistName + " is performing at " + eventVenue + " on " + eventDate + "at " + eventTime);
      	  
-     	  	var spotifyArtistApi = "http://ws.spotify.com/search/1/track?q=artist:"+skArtistName	
+     	  	var spotifyArtistApi = "http://ws.spotify.com/search/1/track?q=artist:"+skArtistName;	
 		
 			var spotifyID = "";
-		
-	
 		
 			var getStuff = $.getJSON(spotifyArtistApi, function(stuffReturned) {
 				console.log( "in success method for the first API call" );
 				console.log("this was retruned from first API call:" + stuffReturned);
 			
+			//success method for page loading....
+
 			//getting tack information for song we want to play
 				var spotifyID = stuffReturned.tracks[0].href;	
 				console.log("this is the href returned " + spotifyID);
@@ -67,15 +87,43 @@
 			//creating URI that will be passed to spotify widget
 				artistInfo =  "http://ws.spotify.com/lookup/1/?uri=" + spotifyID;
 				
+				var holderDiv = document.createElement('div');
+				//holderDiv.style.height = '800px';
+				//holderDiv.style.border = '3px solid black';
+				holderDiv.className = 'holderDiv';
+
+				var testButton = document.createElement('iframe');
+				testButton.id = "SpotifyButton" + i;
+				testButton.class = "spotifyIframe";
+				testButton.width = '300';
+				testButton.height = '380';
+				testButton.frameborder = '0';
+				testButton.allowtransparency = 'true';
+
+				holderDiv.appendChild(testButton);
+				$('#jsonOutput').before(holderDiv);
+
+				var skEventInfo = document.createElement('div');
+				skEventInfo.className= "eventInformation";
+
+				var artistHeader = document.createElement('h2');
+				var artistNode = document.createTextNode(skArtistName);
+
+				skEventInfo.appendChild(artistNode);
+				console.log("this is the name of the artistNode: " + artistNode);
+				//PROBLEM! : why is a single div being added to all of the iframes?
+
+				holderDiv.appendChild(skEventInfo);
+
 			//get spoity widget
-				var firstButton = document.getElementById("firstSpotifyButton");
+			//	var firstButton = document.getElementById("SpotifyButton");
 			
 			//adding SK artist info before spotify widget loads:
 
-				var artistHeader = document.getElementById('artistName');
-				var artistNode = document.createTextNode(skArtistName);
+	
+//				var artistNode = document.createTextNode(skArtistName);
 
-				var date = document.getElementById('eventDate');
+			/*	var date = document.getElementById('eventDate');
 				var dateNode = document.createTextNode('Date: '+ eventDate);
 
 				var venue = document.getElementById('eventVenue');
@@ -84,18 +132,22 @@
 				var city = document.getElementById('eventCity');
 				var cityNode = document.createTextNode('Location: ' + eventLocation);
 			
+			//TODO: need to add a function that erases the content in #Basic Details before it's propogated by new content
+
 			//prints the artist and event info on the page above the listen app
-				artistHeader.appendChild(artistNode);
+
+			/*	artistHeader.appendChild(artistNode);
 				date.appendChild(dateNode);
 				venue.appendChild(venueNode);
-				city.appendChild(cityNode);
+				city.appendChild(cityNode);*/
 
 			//give widget the URI
-				firstButton.src = "https://embed.spotify.com/?uri="+spotifyID;
+				testButton.src = "https://embed.spotify.com/?uri="+spotifyID;
 			
 			   
 			})
-     	  });
+     	  }
+     	});
 
 
     	} else {
